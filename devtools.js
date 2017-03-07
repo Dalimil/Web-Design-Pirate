f28d87560acf8d2f651c34b3650b0980f8a41d32
Dalimil Hajek <dalimilhajek@gmail.com>
2017-03-07 16:51:04 +0000

    Add last inspected element page eval

--- devtools.js
+++ devtools.js
@@ -1,19 +1,33 @@
 
 // Create a new Devtools Panel
-chrome.devtools.panels.create("Pirate",
-    "MyPanelIcon.png",
-    "panel.html",
-    function(thisPanel) {
-      // code invoked on panel creation
+chrome.devtools.panels.create("Pirate", "icon128.png", "panel.html", (thisPanel) => {
+  // code invoked on panel creation
 
-      thisPanel.onShown.addListener((panelWindow) => {
-        console.log("user switched to this panel");
-      });
+  thisPanel.onShown.addListener((panelWindow) => {
+    console.log("user switched to this panel");
+  });
 
-      function onSelectionChanged() {
-        console.log("selection changed");
+  function onSelectionChanged() {
+    console.log("selection changed");
+  }
+  chrome.devtools.panels.elements.onSelectionChanged.addListener(onSelectionChanged);
+
+  getLastInspectedElement().then(result => {
+    console.log("Last inspected element", result);
+  }).catch(() => {
+    console.log("Nothing inspected recently.");
+  });
+});
+
+function getLastInspectedElement() {
+  // Use any CommandLine APIs inside eval() - e.g. $0 or inspect()
+  return new Promise((resolve, reject) => {
+    chrome.devtools.inspectedWindow.eval("$0", (result, exception) => {
+      if (exception || !result) {
+        reject(exception);
+      } else {
+        resolve(result);
       }
-
-      chrome.devtools.panels.elements.onSelectionChanged.addListener(onSelectionChanged);
-    }
-);
+    });
+  });
+}
