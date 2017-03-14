7748fa4c16895269467fd28b62218deb13eed380
Dalimil Hajek <dalimilhajek@gmail.com>
2017-03-14 23:48:10 +0000

    Add background page integration and stylesheet fetching

--- src/devtools.js
+++ src/devtools.js
@@ -1,4 +1,6 @@
-
+// global variables: 
+// contentScripts.getStyleSheets
+// contentScripts.getLastInspectedElement
 const panelFilepath = "src/panel.html";
 const iconFilepath = "images/icon128.png";
 
@@ -10,36 +12,27 @@ chrome.devtools.panels.create("Pirate", iconFilepath, panelFilepath, (thisPanel)
   thisPanel.onShown.addListener(panelWindow => {
     // User switched to this panel
     if (panel == null) {
-      panel = new PanelEnvironment(panelWindow);
+      panel = PanelEnvironment(panelWindow);
     }
     panel.onShown(panelWindow);
   });
 
 });
 
-function getLastInspectedElement() {
-  const expr = `JSON.stringify({
-    element: $0.outerHTML,
-    source: document.documentElement.outerHTML
-  });`;
-  // Use any CommandLine APIs inside eval() - e.g. $0 or inspect()
-  return new Promise((resolve, reject) => {
-    chrome.devtools.inspectedWindow.eval(expr, (result, exception) => {
-      if (exception || !result) {
-        reject(exception);
-      } else {
-        resolve(JSON.parse(result));
-      }
-    });
-  });
-}
-
 function PanelEnvironment(panelWindow) {
   const $ = panelWindow.jQuery;
-  const $loadLastInspected = $("#loadLastInspected");
+  const $pirateElement = $("#pirateElement");
   const $resultDisplay = $("#inspectedResult");
+  let lastInspectedElementHtml = null;
 
-  $loadLastInspected.click(showLastInspected);
+  $pirateElement.prop('disabled', true);
+  $pirateElement.click(() => {
+    if (lastInspectedElementHtml && typeof lastInspectedElementHtml === "string") {
+      pirateElement(lastInspectedElementHtml);
+    }
+  });
+  // update on first panel show
+  showLastInspected();
   // event onElementSelectionChanged
   chrome.devtools.panels.elements.onSelectionChanged.addListener(() => {
     showLastInspected();
@@ -53,12 +46,33 @@ function PanelEnvironment(panelWindow) {
   }
 
   function showLastInspected() {
-    getLastInspectedElement().then(result => {
-      console.log($(result));
+    contentScripts.getLastInspectedElement().then(result => {
+      console.log(result);
+      if (lastInspectedElementHtml == null) {
+        // first time it can be used
+        $pirateElement.prop('disabled', false);
+      }
+      lastInspectedElementHtml = result.element;
       $resultDisplay.html(result.element);
     }).catch(e => {
       console.log(e);
       $resultDisplay.text("Nothing inspected recently.");
+    });
+  }
+
+  let lastProcessFinished = true;
+  function pirateElement(inputHtml) {
+    if (!lastProcessFinished) {
+      console.log("Not yet finished");
+      return;
+    }
+    lastProcessFinished = false;
+    $pirateElement.prop('disabled', true);
+    bgPageConnection.requestStyleSheetsContent(chrome.devtools.inspectedWindow.tabId)
+    .then(styleSheets => bgPageConnection.requestUncss(inputHtml, styleSheets))
+    .then(simplifiedCss => {
+      console.log("done");
+      $pirateElement.prop('disabled', false);
     });
   }
 
@@ -67,4 +81,58 @@ function PanelEnvironment(panelWindow) {
   };
 }
 
+/**
+ * Wrapper for communication with the background.js page
+ */
+const bgPageConnection = (() => {
+  // Create a connection to the background page
+  const backgroundPageConnection = chrome.runtime.connect({
+      name: "devtools-page"
+  });
+
+  let lastCachedStyleSheetsTabId = null;
+  let lastCachedStyleSheetsResponse = null;
+
+  backgroundPageConnection.onMessage.addListener((message) => {
+      // Handle responses from the background page, if any
+      console.log("got response back: ", message);
+  });
+
+  function requestStyleSheetsContent(tabId) {
+    if (lastCachedStyleSheetsResponse !== null && tabId === lastCachedStyleSheetsTabId) {
+      return lastCachedStyleSheetsResponse;
+    }
+    contentScripts.getStyleSheets().then(styleSheets => {
+      return new Promise((resolve, reject) => {
+        chrome.runtime.sendMessage({
+          requestType: "stylesheets",
+          styleSheets
+        }, function(response) {
+          lastCachedStyleSheetsTabId = tabId;
+          lastCachedStyleSheetsResponse = response;
+          console.log(response);
+          resolve(response);
+        });
+      });
+    });
+  }
+
+  function requestUncss(inputHtml, styleSheets) {
+    return new Promise((resolve, reject) => {
+      chrome.runtime.sendMessage({
+        requestType: "uncss",
+        inputHtml,
+        styleSheets
+      }, function(response) {
+        console.log(response);
+        resolve(response);
+      });
+    });
+  }
+
+  return {
+    requestStyleSheetsContent,
+    requestUncss
+  };
+})();
 
