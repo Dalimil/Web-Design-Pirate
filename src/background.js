ae0011d57267d0eb6aaff4725e33f1ff52204c95
Dalimil Hajek <dalimilhajek@gmail.com>
2017-03-14 23:42:55 +0000

    Add background script

---
+++ src/background.js
@@ -0,0 +1,108 @@
+// Background page
+chrome.runtime.onConnect.addListener(function(devToolsConnection) {
+    // assign the listener function to a variable so we can remove it later
+    const devToolsListener = function(message, sender, sendResponse) {
+        if (message.requestType == "stylesheets") {
+          fetchStyleSheetContent(message.styleSheets).then(sendResponse);
+        } else if (message.requestType == "uncss") {
+          getCssForHtml(message.inputHtml, message.styleSheets).then(sendResponse);
+        } else {
+          sendResponse("invalid");
+        }
+        return true; // keep open until sendResponse is called (async)
+    };
+    // add the listener
+    devToolsConnection.onMessage.addListener(devToolsListener);
+
+    devToolsConnection.onDisconnect.addListener(function() {
+         devToolsConnection.onMessage.removeListener(devToolsListener);
+    });
+});
+
+/** Fetching CSS stylesheets from anywhere
+ *  -> CORS issue so it has to be done in background.js
+ */
+function fetchStyleSheetContent(styleSheets) {
+  return Promise.all(styleSheets.map(sheet => {
+    if (sheet.cssText) {
+      return Promise.resolve(sheet.cssText);
+    } else if (sheet.href) {
+      return fetch(sheet.href).then(data => data.text());
+    }
+    return Promise.resolve(null);
+  })).then(values => {
+    // zip hrefs back and filter out nulls
+    return values.map((v, i) => {
+      return {
+        source: styleSheets[i].href,
+        cssText: v
+      };
+    }).filter(x => x.cssText != null);
+  });
+}
+
+
+/**
+ * Merge all stylesheets, remove unneeded CSS by calling uncss api,
+ * return simplified css string and stats about usage of individual sources
+ */
+function getCssForHtml(inputHtml, styleSheets) {
+	const separator = "/* ---sep--- */";
+	const blockCommentRegexp = /\/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+\//g;
+
+	return styleSheets.then(values => {
+		// values = [ { source: "a.css", cssText: "body ..." }, ...]
+
+		const allCss = values.map(x => x.cssText).join(separator);
+
+		return _uncss(inputHtml, allCss).then(outputCss => {
+			const cssPieces = outputCss.split(separator);
+			if (cssPieces.length != values.length) {
+				throw new Error("API-returned CSS is not in expected format.");
+			}
+			return cssPieces.map((v, i) => ({
+				source: values[i].source,
+				cssText: v.replace(blockCommentRegexp, "").trim()
+			}));
+		}).then(cssPieces => {
+			const stats = cssPieces.map(({ source, cssText }) => ({
+				source,
+				usage: cssText.length
+			}));
+
+			const cssString = cssPieces.map(({ source, cssText }) =>
+				`/** ----- ${source} ----- */\n${cssText}`).join("\n");
+
+			return {
+				stats,
+				css: cssString
+			};
+		});
+	});
+}
+
+/**
+ * Return simplified CSS (based on given HTML and CSS)
+ * Use test API: https://github.com/giakki/uncss 
+ *
+ * Alternative solution: Use https://github.com/reworkcss/css
+ * 	and document.querySelector() each of the CSS selectors to check if result is null
+ */
+function _uncss(inputHtml, inputCss) {
+	const testServerUrl = "https://uncss-online.com/uncss";
+	const formData = new FormData();
+	formData.append("inputHtml", inputHtml);
+	formData.append("inputCss", inputCss);
+	formData.append("type", "fetch");
+	return fetch(testServerUrl, {
+		method: "POST",
+		body: formData
+	}).then(data => data.json())
+	.then(data => {
+		if (data.error) {
+			throw data.error;
+		}
+		return data.outputCss;
+	});
+}
+
